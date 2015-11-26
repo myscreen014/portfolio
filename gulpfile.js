aaec3dddf6b8ecdc5ad45165b74f7de48f9b301c
myscreen014 <eric.tarillon@gmail.com>
2015-11-26 20:00:05 +0100

    Resend scss

--- gulpfile.js
+++ gulpfile.js
@@ -14,7 +14,12 @@ elixir.config.sourcemaps = false;
  */
 
 elixir(function(mix) {
-	
+
+    /*
+    |--------------------------------------------------------------------------
+    | Admin
+    */
+    
     /* COPY CSS files */
     mix.copy('resources/assets/css/bootstrap.min.css', 'public/css/bootstrap.min.css');
     mix.copy('resources/assets/css/font-awesome.min.css', 'public/css/font-awesome.min.css');
@@ -23,7 +28,7 @@ elixir(function(mix) {
     mix.copy('resources/assets/img', 'public/img');
 
     /* SASS */
-	mix.sass([
+    mix.sass([
         'mixins.scss',
         'admin.scss',
         'sb-admin-2.scss',
@@ -33,19 +38,39 @@ elixir(function(mix) {
     ], "public/css/wysiwyg.css");
 
     mix.scripts([
-    	'jquery-1.11.3.min.js',
+        'jquery-1.11.3.min.js',
         'jquery-ui.min.js',
         'bootstrap.min.js',
-    	'dropzone.js',
+        'dropzone.js',
         'admin.js',
         'admin.modal.js'
     ], "public/js/admin.all.js" );
    
+    
+
+     /*
+    |--------------------------------------------------------------------------
+    | Site
+    */
+    mix.sass([
+        'mixins.scss',
+        'site.grid.scss',
+        'site.scss',
+    ], "public/css/site.all.css");
+
+    mix.scripts([
+        'jquery-1.11.3.min.js',
+        'site.js',
+    ], "public/js/site.all.js" );
+
+    
+
     mix.version([
         "public/css/bootstrap.min.css",
         "public/css/font-awesome.min.css",
         "public/css/admin.all.css",
-        "public/js/admin.all.js"
+        "public/js/admin.all.js",
+        "public/js/site.all.js",
+        "public/css/site.all.css",
     ]);
-
 });
