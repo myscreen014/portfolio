dd760f10b18aa059136e7138a4e632ed4e0a1f33
myscreen014 <eric.tarillon@gmail.com>
2015-12-18 14:58:26 +0100

    Add Google Analytics

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -8,13 +8,30 @@
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 		<meta name="description" content="">
 		<meta name="author" content="">
-		<link rel="icon" href="../../favicon.ico">
+		<link rel="icon" href="favicon.ico">
+
+		<meta name="copyright" content="(c) Eric Tarillon">
+		<meta name="author" content="{{ trans('site.global.name') }}">
+		<meta name="generator" content="Laravel" />
+
 		<title>{{ trans('site.global.name') }}</title>
 		
 		<link href='https://fonts.googleapis.com/css?family=Quicksand:400,300,700' rel='stylesheet' type='text/css'>
 		<link rel="stylesheet" href="{{ elixir('css/site.all.css') }}">
 		<link rel="stylesheet" href="{{ asset('plugins/lightbox/jquery.fancybox.css') }}">
 		<link rel="stylesheet" href="{{ asset('css/font-awesome.min.css') }}">
+
+		{{-- Google analytics --}} 
+		@if (Config::get('app.ga_code'))
+			<script>
+				(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	  			ga('create', "{{ Config::get('app.ga_code') }}", 'auto');
+	  			ga('send', 'pageview');
+			</script>
+		@endif
 
 	</head>
 
