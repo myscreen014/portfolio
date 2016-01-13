cb45aaaea58c262093c4448f0bbcf3447fcd8431
myscreen014 <eric.tarillon@gmail.com>
2016-01-13 20:47:54 +0100

    Bugfix path favicon

--- resources/views/site.blade.php
+++ resources/views/site.blade.php
@@ -8,7 +8,7 @@
 		<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
 		<meta name="description" content="">
 		<meta name="author" content="">
-		<link rel="icon" href="favicon.ico">
+		<link rel="icon" href="{{ asset('favicon.png') }}">
 
 		<meta name="copyright" content="(c) Eric Tarillon">
 		<meta name="author" content="{{ trans('site.global.name') }}">
