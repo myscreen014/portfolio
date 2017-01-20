ed315cdd1339032b2baebcd1366f7edba01d1375
Eric <eric.tarillon@gmail.com>
2017-01-20 21:47:40 +0100

    Thumbnails

--- config/thumbnail.php
+++ config/thumbnail.php
@@ -68,7 +68,7 @@ return [
 		'zoom' => [
 			'filters' => [
 				'max' 	=>  [1920,1080],
-				'watermark'	=> ['img/watermark.png', 'bottom-right', 30, 10]
+				'watermark'	=> ['img/watermark.png', 'bottom-right', 20, 10]
 			],
 			'quality' => 100
 		],
